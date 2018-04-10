a33c961380d4b44c2e5536766a4c3c49c398041f
Ilya Shmelev <ishmelev23@gmail.com>
2018-04-10 08:55:43 +0300

    Changed interface for transfers provider

--- gateway/src/account/base_account_transfers_provider.py
+++ gateway/src/account/base_account_transfers_provider.py
@@ -1,7 +1,4 @@
 class BaseAccountTransfersProvider:
-    def __get_latest_transaction_id(self):
-        raise NotImplementedError()
-
     def fetch_all_transactions(self):
         raise NotImplementedError()
 
