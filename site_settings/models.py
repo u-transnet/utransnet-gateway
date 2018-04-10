fc6aa53f59e289809f84a9f1d7bd8893f203a421
Ilya Shmelev <ishmelev23@gmail.com>
2018-04-10 08:38:10 +0300

    Added help_text for site_settings

--- site_settings/models.py
+++ site_settings/models.py
@@ -4,15 +4,22 @@ from django.utils.translation import gettext as __
 
 class SettingsModel(models.Model):
 
-    bitshares_transnet_gateway_address = models.CharField(verbose_name=_('Address'), max_length=255)
+    bitshares_transnet_gateway_address = models.CharField(verbose_name=_('Address'), max_length=255,
+                                                          help_text=_('Address of the account '
+                                                                      'that will be used as gateway in BitShares'))
     bitshares_transnet_active_wif = models.CharField(verbose_name=_('Active key (WIF)'), max_length=255)
     bitshares_transnet_memo_wif = models.CharField(verbose_name=_('Memo key (WIF)'), max_length=255)
-    bitshares_transnet_node_url = models.CharField(verbose_name=_('URL of node'), max_length=255)
+    bitshares_transnet_node_url = models.CharField(verbose_name=_('URL of node'), max_length=255,
+                                                   help_text=_('URL of the node for interacting with BitShares'))
 
-    transnet_bitshares_gateway_address = models.CharField(verbose_name=_('Address'), max_length=255)
+    transnet_bitshares_gateway_address = models.CharField(verbose_name=_('Address'), max_length=255,
+                                                          help_text=_('Address of the account '
+                                                                      'that will be used as gateway in Transnet')
+                                                          )
     transnet_bitshares_active_wif = models.CharField(verbose_name=_('Active key (WIF)'), max_length=255)
     transnet_bitshares_memo_wif = models.CharField(verbose_name=_('Memo key (WIF)'), max_length=255)
-    transnet_bitshares_node_url = models.CharField(verbose_name=_('URL of node'), max_length=255)
+    transnet_bitshares_node_url = models.CharField(verbose_name=_('URL of node'), max_length=255,
+                                                   help_text=_('URL of the node for interacting with Transnet'))
 
     def __str__(self):
         return __('Settings')
